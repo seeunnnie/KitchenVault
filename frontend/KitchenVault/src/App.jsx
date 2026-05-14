88924cbc62742518f0a2d13f08f11534c8b668fe
seeunnnie <weseeuneb@gmail.com>
2026-05-13 23:59:47 -0400

    frontend init

---
+++ frontend/KitchenVault/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Dashboard from './components/dashboard'
+
+function App() {
+
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/KitchenVault/src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Dashboard() {
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500"> 
+      <h1 className="text-white text-4xl font-bold">This is Dashboard</h1> 
+    </div>
+  )
+}
+
+export default Dashboard

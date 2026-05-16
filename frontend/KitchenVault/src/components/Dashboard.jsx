e7c2286288268345deac2243bac2989e0375f2b8
seeunnnie <weseeuneb@gmail.com>
2026-05-16 15:40:03 -0400

    created navbar

--- frontend/KitchenVault/src/components/Dashboard.jsx
+++ frontend/KitchenVault/src/components/Dashboard.jsx
@@ -4,6 +4,7 @@ function Dashboard() {
 
   return (
     <div className="flex items-center justify-center h-screen bg-blue-500"> 
+      <Navbar />
       <h1 className="text-white text-4xl font-bold">This is Dashboard</h1> 
     </div>
   )
---
+++ frontend/KitchenVault/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Navbar() {
+
+  return (
+    <div className="flex items-center justify-between p-4 bg-gray-800 text-white">
+        </div>
+  )
+}
+
+export default Navbar
+
